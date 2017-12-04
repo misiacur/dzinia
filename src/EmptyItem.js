984118dbda8842523ce82988a47c171e951801e9
misiacur <michael.trzeciak@gmail.com>
2017-12-04 22:13:54 +0100

    dzinia initial commit

---
+++ src/EmptyItem.js
@@ -0,0 +1,12 @@
+class EmptyItem {
+    static get EMPTY_ITEM() {
+        return {
+            name: "",
+            carbs: 0,
+            weight: 100,
+            fat: 0,
+            calories: 0,
+            proteins: 0
+          }; 
+    }
+}
